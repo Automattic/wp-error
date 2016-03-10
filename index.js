8d57245ce5a2f634576fba292c4187babb452fc6
Nathan Rajlich <nathan@tootallnate.net>
2016-03-09 18:58:48 -0800

    add built-in HTTP status code functionality
    Common logic since `wpcom-proxy-request` and
    `wpcom-xhr-request` will use the same logic for that.

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 var uppercamelcase = require('uppercamelcase');
+var statusCodes = require('builtin-status-codes');
 
 module.exports = WPError;
 
-function WPError (data) {
+function WPError () {
   var self = new Error();
 
-  if (data.error) {
-    self.name = toTitle(data.error);
-  }
-
-  if (data.error_description) {
-    self.message = data.error_description;
-  }
-
-  for (var i in data) {
-    self[i] = data[i];
+  for (var i = 0; i < arguments.length; i++) {
+    process(self, arguments[i]);
   }
 
   if (typeof Error.captureStackTrace === 'function') {
@@ -24,6 +17,54 @@ function WPError (data) {
   return self;
 }
 
-function toTitle (str) {
-  return uppercamelcase(String(str).replace(/error$/i, ''), 'error');
+function process ( self, data ) {
+  if (typeof data === 'number') {
+    // just a status code then 1  q
+    setStatusCode( self, data );
+
+  } else {
+    // assume it's a plain 'ol Object with some props to copy over
+    if (data.error) {
+      self.name = toName(data.error);
+    }
+
+    if (data.error_description) {
+      self.message = data.error_description;
+    }
+
+    for (var i in data) {
+      self[i] = data[i];
+    }
+
+    if (self.status && (data.method || data.path)) {
+      setStatusCodeMessage( self );
+    }
+  }
+}
+
+function setStatusCode ( self, code ) {
+  self.name = toName( statusCodes[ code ] );
+  self.status = self.statusCode = code;
+  setStatusCodeMessage( self );
+}
+
+function setStatusCodeMessage ( self ) {
+  var code = self.status;
+  var method = self.method;
+  var path = self.path;
+
+  var m = code + ' status code';
+  var extended = method || path;
+
+  if ( extended ) m += ' for "';
+  if ( method ) m += method;
+  if ( extended ) m += ' ';
+  if ( path ) m += path;
+  if ( extended ) m += '"';
+
+  self.message = m;
+}
+
+function toName ( str ) {
+  return uppercamelcase( String(str).replace(/error$/i, ''), 'error' );
 }
