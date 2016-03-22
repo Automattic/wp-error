76e060982d7bed77d8fada8c14cc0c1b78b3bab4
Nathan Rajlich <nathan@tootallnate.net>
2016-03-22 11:44:12 -0700

    test: test for `statusCode`

--- test.js
+++ test.js
@@ -17,6 +17,7 @@ assert.equal(err.error, 'processing_failed');
 err = new WPError(404);
 assert.equal(err.name, 'NotFoundError');
 assert.equal(err.message, '404 status code');
+assert.equal(err.statusCode, 404);
 
 
 // chain multiple objects / status code
@@ -25,12 +26,14 @@ assert.equal(err.foo, 'bar');
 assert.equal(err.name, 'MyError');
 assert.equal(err.error, 'my_error');
 assert.equal(err.message, '403 status code');
+assert.equal(err.statusCode, 403);
 
 
 // chain multiple objects / status code
 err = new WPError(500, { message: 'wow' });
 assert.equal(err.name, 'InternalServerError');
 assert.equal(err.message, 'wow');
+assert.equal(err.statusCode, 500);
 
 
 // error from OAuth "Deny" button
